2cf40febaf579f0528dde932c9f243e74bce5b8c
muratgezici <101368023+muratgezici@users.noreply.github.com>
2024-09-13 00:03:26 +0300

    Audio Settings Implemented

--- Assets/Scripts/UI/CMainMenuActions.cs
+++ Assets/Scripts/UI/CMainMenuActions.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class CMainMenuActions : MonoBehaviour
 {
+    [SerializeField] private GameObject SettingsPanel;
+    private bool IsSettingsPanelOpened = false;
     public void OnStartGameClicked()
     {
         EventManager.TransitionStart("UITestScene");
+    }
+    public void OnSettingsClicked()
+    {
+        IsSettingsPanelOpened = !IsSettingsPanelOpened;
+        SettingsPanel.SetActive(IsSettingsPanelOpened);
     }
 }
--- Assets/Scripts/UI/CSettingsActions.cs
+++ Assets/Scripts/UI/CSettingsActions.cs
@@ -1,12 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CSettingsActions : MonoBehaviour
 {
     [SerializeField] private GameObject MusicSettingsObject;
     [SerializeField] private GameObject SoundEffectSettingsObject;
 
+    private void Start()
+    {
+        if(PlayerPrefs.GetFloat("MusicSettings") != 0)
+        {
+            MusicSettingsObject.GetComponent<Slider>().value = PlayerPrefs.GetFloat("MusicSettings");
+        }
+        if(PlayerPrefs.GetFloat("SoundEffectSettings") != 0)
+        {
+            SoundEffectSettingsObject.GetComponent<Slider>().value = PlayerPrefs.GetFloat("SoundEffectSettings");
+        }
+        
 
+    }
+    public void OnMusicSettingsChanged()
+    {
+        float value = MusicSettingsObject.GetComponent<Slider>().value;
+        PlayerPrefs.SetFloat("MusicSettings", value);
+    }
+    public void OnSoundEffectSettingsChanged()
+    {
+        float value = SoundEffectSettingsObject.GetComponent<Slider>().value;
+        PlayerPrefs.SetFloat("SoundEffectSettings", value);
+    }
 
 }
